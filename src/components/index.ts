52d3d1e1ef3ff1ee60a8d79148378fb1644fb25a
powerty <qwercy142@gmail.com>
2021-07-20 21:34:22 +0200

    Absolute imports

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
+
+export { default as Animation } from "./Animation";
+export { default as Section } from "./Section";
+
+export { default as Hero } from "./Hero/Hero";
+export { default as About } from "./About/About";
+export { default as Projects } from "./Projects/Projects";
+export { default as Contact } from "./Contact/Contact";
+
+export { default as Services } from "./Services";
+export { default as Acomplishments } from "./Acomplishments";
+
+export * from "./Typography";
