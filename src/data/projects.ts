e5c6509f5c70a7622d5ad698da5e7c4665541f87
powerty <qwercy142@gmail.com>
2021-07-20 21:35:26 +0200

    Create data

---
+++ src/data/projects.ts
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    id: "0",
+    name: "Notefy",
+    description:
+      "Make own note in less than 10 seconds and share unique link for your message to your friends",
+    type: "PWA",
+    tech_stack: ["Next.js", "Firebase", "TypeScript", "Styled-Components"],
+    tags: [
+      "nookies",
+      "framer-motion",
+      "react-icons",
+      "nProgress",
+      "mobile frendly",
+    ],
+    code: "https://github.com/kubo550/notefy",
+    preview: "https://",
+    image: "/images/projects/img2.png",
+  },
+  {
+    id: "1",
+    name: "Dev Blog",
+    description:
+      "Blog for evry programmer who live alone to improve Your coding and cooking skills. Blog how to coode and how to cook all in one place.",
+    type: "PWA",
+    tech_stack: [
+      "Next.js",
+      "HeadlesCMS",
+      "TypeScript",
+      "TailwindCSS",
+      "MarkDown",
+    ],
+
+    tags: [
+      "framer-motion",
+      "react-icons",
+      "nProgress",
+      "dark mode",
+      "react-scroll",
+      "responsive",
+    ],
+    code: "https://github.com/kubo550/dev-blog",
+    preview: "https://",
+    image: "/images/projects/img3.png",
+  },
+];
